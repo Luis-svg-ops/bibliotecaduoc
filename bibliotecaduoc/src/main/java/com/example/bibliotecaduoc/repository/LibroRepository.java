64e76e71f0541570a6a2c36ca3b8a7022a3d134a
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-03-24 11:26:03 -0300

    repository.java agrgdo

---
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -0,0 +1,17 @@
+package com.example.bibliotecaduoc.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import com.example.bibliotecaduoc.model.Libro;
+
+
+
+@Repository
+
+public class LibroRepository {
+    
+    private List<Libro> listoLibros=new ArrayList<>();
+
+}
